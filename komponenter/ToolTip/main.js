9414fc983e8fb222ec1576784771ffd8ca0d40fb
SandraEsp <sandra-espensen@hotmail.com>
2019-04-14 16:40:53 +0200

    Changed var to let

--- komponenter/ToolTip/main.js
+++ komponenter/ToolTip/main.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", function () {
-    var bokse = document.getElementsByClassName("bokse");
-    for (var i = 0; i < bokse.length; i++) {
+    let bokse = document.getElementsByClassName("bokse");
+    for (let i = 0; i < bokse.length; i++) {
         bokse[i].addEventListener("mouseover", function () {
-            var tooltip = this.getElementsByClassName("tooltip")[0];
+            let tooltip = this.getElementsByClassName("tooltip")[0];
             tooltip.removeAttribute("style");
         });
         bokse[i].addEventListener("mouseout", function () {
-            var tooltip = this.getElementsByClassName("tooltip")[0];
+            let tooltip = this.getElementsByClassName("tooltip")[0];
             tooltip.style.display = "none";
         });
     }
